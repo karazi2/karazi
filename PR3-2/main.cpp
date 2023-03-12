b4e7faebdb14d0cb76ad73fd7ac3ba96eaded068
karazi2 <125014696+karazi2@users.noreply.github.com>
2023-03-12 18:18:47 +0300

    Add files via upload

---
+++ PR3-2/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "rational.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int size;
+	cout << "Введите количество дробей: "; cin >> size;
+	cout << "\n";
+	rational* arr = new rational[size];
+	int a, b;
+
+	cout << "Вводите числитель и знаменатель через пробел \n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Дробь номер " << i + 1 << " : ";
+		cin >> a >> b;
+		(arr[i]).set(a, b);
+	}
+	cout << "\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\nВывод дроби " << i + 1 << " : ";
+		(arr[i]).show();
+	}
+	cout << "\n";
+
+	delete[] arr;
+
+	return 0;
+}
