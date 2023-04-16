f9119221b251f9f39b51019cd8aa2c3a43fb3ea2
karazi2 <125014696+karazi2@users.noreply.github.com>
2023-04-16 19:47:58 +0300

    Add files via upload

---
+++ PR8/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "code.h"
+
+int main() {
+    type3 eq3(7.0);
+    eq3.show();
+    eq3.Get_answer();
+
+    type4 eq4(2.0, 5.0);
+    eq4.show();
+    eq4.Get_answer();
+
+    return 0;
+}
